9fc947f26fc50e10c265fccdb36a5f4a76526e9a
Kuro <68709896+kurowyn@users.noreply.github.com>
2025-05-24 19:52:21 +0100

    Update main.c
    A great amount of changes.
    
    - Fixed bug where a file is said to not exist (even when it perfectly exists).
    - Implemented shared memory.
    - Output comes in the same order of the files.
    - Some refactoring (more to come soon).

--- main.c
+++ main.c
@@ -1,104 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <wait.h>
-#include <errno.h>
-
-#define DYNAMIC_READ
-//#define STATIC_READ
-
-#if defined(STATIC_READ)
-    #define MAX_BUFFER_LEN 1024
-#endif
-
-#define ERR_HANDLE(err_cond, err_msg, err_code, ...) \
-    if (err_cond) { \
-        fprintf(stderr, err_msg, __VA_ARGS__); \
-        exit(err_code); \
-    } \
-
-enum {WORD_FOUND = 0,
-      WORD_NOT_FOUND = 1,
-      FILE_NOT_FOUND = 2};
-
-char *
-dynamic_read(const char *filename){
-    char *buffer = malloc(0);
-    FILE *f = fopen(filename, "r");
-
-    ERR_HANDLE(buffer == NULL, 
-               "ERROR: could not allocate memory, errno: %d",
-               EXIT_FAILURE,
-               errno);
-
-    ERR_HANDLE(f == NULL,
-               "ERROR: could not open file %s, errno: %d\n",
-               errno == ENOENT ? FILE_NOT_FOUND : errno,
-               filename, errno);
-
-    // ENOENT: file doesn't exit. (check manpage for errno)
-    // This is to filter out the cases in which the file does exist,
-    // but for some reason could not be opened. In that case, we exit
-    // with the errno code.
-
-    for (;;) {
-        static int i = 0, c;
-        if ((c = fgetc(f)) != EOF) {
-            // This is for the unlikely event we encounter 
-            // NULL characters in our file.
-            if (c != '\0') {
-                buffer = realloc(buffer, (++i) * sizeof (char));
-
-                ERR_HANDLE(buffer == NULL, 
-                           "ERROR: could not reallocate memory, errno: %d",
-                           EXIT_FAILURE,
-                           errno);
-
-                buffer[i - 1] = c;
-            } 
-        } else {
-                buffer[i - 1] = '\0';
-                break;
-        }
-    }
-
-    return buffer;
-}
-
-int 
-search_file(const char *filename, const char *token){
-#if defined(DYNAMIC_READ)
-    char *buffer = dynamic_read(filename);
-
-    int found = strstr(buffer, token) != NULL ? WORD_FOUND : WORD_NOT_FOUND;
-
-    free(buffer);
-
-    return found;
-#elif defined(STATIC_READ)
-    FILE *f = fopen(filename, "r");
-
-    ERR_HANDLE(f == NULL,
-               "ERROR: could not open file %s, errno: %d\n",
-               errno == ENOENT ? FILE_NOT_FOUND : errno,
-               filename, errno);
-
-    char buffer[MAX_BUFFER_LEN];
-
-    while (fgets(buffer, MAX_BUFFER_LEN, f) != NULL) {
-        if (strstr(buffer, token) == NULL) {
-            continue;
-        }
-        return WORD_FOUND;
-    }
-
-    return WORD_NOT_FOUND;
-#endif
-}
+#include "utils.h"
 
 int main(int argc, char **argv){
     int file_count = argc - 2;
+
+    int *file_statuses = mmap(NULL, 
+                              file_count * sizeof(int),
+                              PROT_READ | PROT_WRITE, 
+                              MAP_SHARED | MAP_ANONYMOUS,
+                              -1,
+                              0);
+
+    for (int i = 0; i < file_count; ++i)
+        file_statuses[i] = -1;
+
+    char **files = argv + 2;
+
     char *word = argv[1];
 
     if (argc < 3) {
@@ -109,42 +25,43 @@ int main(int argc, char **argv){
     // Now, we make the processes.
 
     for (size_t i = 0; i < file_count; ++i) {
-        if (fork() == 0) {
-            // We're at the child process now.
-            // We look for the word in the ith process.
-            // We then exit appropriately.
-            // Note: it may not be as clear, but search_file()
-            // handles abnormal exit codes other than WORD_FOUND and
-            // WORD_NOT_FOUND, so the exit code can be something other
-            // than those two in case something weird happens.
-            exit(search_file(argv[i + 2], word) == WORD_FOUND ?
-                 WORD_FOUND : WORD_NOT_FOUND);
-        }
+        int fork_val = fork();
+        ERR_HANDLE(fork_val == -1,
+                   "ERROR: could not fork process; errno: %d",
+                   exit(errno),
+                   errno);
+        if (fork_val == 0) {
+            // The search_file function now returns an exit code,
+            // instead of the function itself exiting.
+            // This is still safe.
+            file_statuses[i] = search_file(files[i], word);
+            exit(file_statuses[i]);
+        } 
     }
 
     for (size_t i = 0; i < file_count; ++i) {
         int child_status;
         wait(&child_status);
+
         if (WIFEXITED(child_status)) {
-            // I hear switches are much faster than ifs.
-            switch (WEXITSTATUS(child_status)) {
+            switch (file_statuses[i]) {
                 case WORD_FOUND:
-                    printf("Word %s found in %s.\n",
-                           word, argv[i + 2]);
+                    printf("%d %s: %s found.\n",
+                           i + 1, files[i], word);
                     break;
                 case WORD_NOT_FOUND:
-                    printf("Word %s not found in %s.\n",
-                           word, argv[i + 2]);
+                    printf("%d %s: %s not found.\n",
+                           i + 1, files[i], word);
                     break;
                 case FILE_NOT_FOUND:
-                    printf("ERROR: %s: no such file.\n",
-                           argv[i + 2]);
+                    printf("%d %s: no such file.\n",
+                           i + 1, files[i]);
                     break;
                 default:
                     fprintf(stderr, 
-                            "ERROR: unknown error occured (exit status: %d).\n"
+                            "ERROR: unknown error occured (exit statuses: %d, %d).\n"
                             "Contact your system adminstrator.\n",
-                            WEXITSTATUS(child_status));
+                            file_statuses[i], WEXITSTATUS(child_status));
                     break;
             }
         }
