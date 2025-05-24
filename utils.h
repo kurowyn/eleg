1e467adb49e0b0bd7fc6d881d2bc188a3fef059d
Kuro <68709896+kurowyn@users.noreply.github.com>
2025-05-24 21:39:52 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -2,6 +2,7 @@
 #define UTILS_H
 
 #include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,18 +17,35 @@
 #if defined(VERBOSE_ERR_HANDLE) 
     #define ERR_HANDLE(err_cond, err_msg, err_handle, ...) \
         if (err_cond) {fprintf(stderr, err_msg, __VA_ARGS__); err_handle;}
-    #elif !defined(VERBOSE_ERR_HANDLE) || defined(SILENT_ERR_HANDLE)
-        #define ERR_HANDLE(err_cond, err_msg, err_handle, ...) \
-            if (err_cond) {err_handle;}
+#elif !defined(VERBOSE_ERR_HANDLE) || defined(SILENT_ERR_HANDLE)
+    #define ERR_HANDLE(err_cond, err_msg, err_handle, ...) \
+        if (err_cond) {err_handle;}
 #endif 
 
-enum {WORD_FOUND = 0,
+#define MIN_ARG_COUNT 3
+
+// The NO_STATUS one actually makes sense, since it means something went so
+// wrong that we couldn't even check whether the word is even in the file.
+// Also used when we intialize the FileStatusList - we haven't yet done 
+// anything.
+enum {NO_STATUS = -1,
+      WORD_FOUND = 0,
       WORD_NOT_FOUND = 1,
-      FILE_NOT_FOUND = ENOENT};
-// Check errno manpage.
+      FILE_NOT_FOUND = ENOENT,};
+// Check errno manpage for the last one.
 
-char *dynamic_read(const char *filename);
+typedef struct FileStatus FileStatus;
 
-int search_file(const char *filename, const char *token);
+struct FileStatus {
+    int8_t status;
+    char *name;
+};
+
+FileStatus *FileStatusListInit(char **file_list, uint64_t file_count);
+
+// Might replace this with getline soon.
+char *dynamic_read(const char *file_name);
+
+int search_file(const char *file_name, const char *token);
 
 #endif // UTILS_H
