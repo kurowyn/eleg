4921108625502399b61d06ea03857685882d374f
Kuro <68709896+kurowyn@users.noreply.github.com>
2025-05-29 20:54:15 +0100

    Update parsearg.h

--- parsearg.h
+++ parsearg.h
@@ -3,11 +3,6 @@
 
 #include "utils.h"
 
-#define PROGRAM_NAME "./eleg"
-#define FILE_LIST (argv + 1)
-#define FILE_COUNT (argc - 1)
-#define TARGET_WORD (argv[0])
-
 void UsageMessage(void);
 void VersionMessage(void);
 char **ParseArgList(int *argc, char **argv);
