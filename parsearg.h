9467bf894f7d8179c0a61ff1168a8e6c2a933bba
Kuro <68709896+kurowyn@users.noreply.github.com>
2025-05-29 17:02:55 +0100

    Create parsearg.h

---
+++ parsearg.h
@@ -0,0 +1,17 @@
+#ifndef PARSEARG_H
+#define PARSEARG_H
+
+#include "utils.h"
+
+#define PROGRAM_NAME "./eleg"
+#define FILE_LIST (argv + 1)
+#define FILE_COUNT (argc - 1)
+#define TARGET_WORD (argv[0])
+
+void UsageMessage(void);
+
+void VersionMessage(void);
+
+char **ParseArgList(int *argc, char **argv);
+
+#endif // PARSEARG_H
