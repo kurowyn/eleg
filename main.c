74643a10f2d485e0daab75e7e66876295d069446
Kuro <68709896+kurowyn@users.noreply.github.com>
2025-05-29 17:00:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,93 +1,34 @@
-#include "utils.h"
+#include "eleg.h" 
+#include "parsearg.h"
 
-// Putting this in a different file can make things a little too abstracted,
-// so it's here directly.
-#define PROGRAM_NAME argv[0]
-#define TARGET_WORD argv[1]
-#define FILE_COUNT argc - 2
-#define FILE_LIST argv + 2
+int 
+main(int argc, char **argv){
+    argv = ParseArgList(&argc, argv);
 
-// foz
-void help(){
+    FileStatus *file_status_list = FileStatusListInit(FILE_LIST, FILE_COUNT); 
 
-    printf("Usage: eleg [OPTION]... WORD [FILE]... \n\n");
-    printf("Elementary implementation of grep in the C programming language (thus the name, eleg).\n\n");
-    printf("Options:\n");
-    printf("  -h, --help\tshow help\n\n");
-
-
-}
-
-// end foz
-
-int  main(int argc, char **argv){
-
-    //foz
-    
-    if (argc > 1 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
-        help();
-        return EXIT_SUCCESS;
-    }
-
-
-    // end foz
-
-
-    if (argc < MIN_ARG_COUNT) {
-        fprintf(stderr, "usage: %s (word) (FILE(s))", PROGRAM_NAME);
-        exit(EXIT_FAILURE);
-    }
-
-    FileStatus *FileStatusList = FileStatusListInit(FILE_LIST, FILE_COUNT); 
-
-    // Now, we make the processes.
-    for (size_t i = 0; i < FILE_COUNT; ++i) {
+    // Now, we make the children.
+    for (ssize_t i = 0; i < FILE_COUNT; ++i) {
         pid_t fork_val = fork();
         ERR_HANDLE(fork_val == -1,
                    "ERROR: could not fork process; errno: %d",
                    exit(errno),
                    errno);
         if (fork_val == 0) {
-            // The search_file function now returns the exit code,
-            // instead of the function(s) itself (themselves) exiting.
-            // This is still safe.
-            // Yes, this code is what it looks like.
-            exit(FileStatusList[i].status = search_file(FileStatusList[i].name,TARGET_WORD));
+            // Yes, this code is correct. 
+            exit(file_status_list[i].status =
+                 FileSearch(FILE_LIST[i], TARGET_WORD));
         }
     }
 
     // This is so that the parent doesn't act while a child process is still
     // running. The status list analysis begins only after all the children
-    // terminate. Check the manpage for more info.
+    // terminate. Otherwise, wrong output is generated.
     while (wait(NULL) >= 0);
 
-    for (size_t i = 0; i < FILE_COUNT; ++i) {
-        switch (FileStatusList[i].status) {
-            case WORD_FOUND:
-                printf("%d %s: %s found.\n",
-                       i + 1, FileStatusList[i].name, TARGET_WORD);
-                break;
-            case WORD_NOT_FOUND:
-                printf("%d %s: %s not found.\n",
-                       i + 1, FileStatusList[i].name, TARGET_WORD);
-                break;
-            case FILE_NOT_FOUND:
-                printf("%d %s: no such file.\n",
-                       i + 1, FileStatusList[i].name);
-                break;
-            default:
-                // Under normal circumstances, this should not happen,
-                // except if search_file() returns an unordinary exit code.
-                // (which means, practically speaking, it shouldn't 
-                // ever occur). 
-                fprintf(stderr, "ERROR: unkown error occurred.\n"
-                                "File status: %d, errno: %d.\n"
-                                "Contact your system adminstrator.\n",
-                                FileStatusList[i].status, errno);
-                break;
-        }
-    }
+    FileShowStatusList(TARGET_WORD, file_status_list, FILE_COUNT);
+
+    CLEAN_UP(argv, file_status_list);
 
     return EXIT_SUCCESS;
 }
-
